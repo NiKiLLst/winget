56025cb0621789acb635f28507c5cf059dc63398
nikillst <filippo.ferretti@gmail.com>
2025-03-17 13:20:10 +0100

    FIX vari per versione1

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%winget.ps1
+SET PowerShellScriptPath=%ThisScriptsDirectory%wingetV4.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
