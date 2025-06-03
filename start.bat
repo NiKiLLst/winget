9f66e31ca75455c2694040071c28f0bac783975e
filippo.ferretti <filippo.ferretti@gmail.com>
2025-06-03 18:12:00 +0200

    Versione Finale

--- start.bat
+++ start.bat
@@ -1,4 +1,13 @@
 @ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%wingetV4.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
+:: Mappa il percorso UNC a un'unità temporanea
+pushd %~dp0
+
+:: Salva il percorso attuale dopo il mapping
+SET ThisScriptsDirectory=%CD%
+SET PowerShellScriptPath=%ThisScriptsDirectory%\wingetV4.ps1
+
+:: Esegui PowerShell con privilegi elevati
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}"
+
+:: Rilascia l'unità temporanea assegnata a UNC
+popd
